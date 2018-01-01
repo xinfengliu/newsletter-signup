1bbd0973aba6724a1784648318ec988edfedf0cc
Xinfeng Liu <you@example.com>
2018-01-01 22:40:22 +0800

    refine sqlserver dockerfile and init script (allow volume mount, specify db name)

--- docker/db/init.ps1
+++ docker/db/init.ps1
@@ -1,19 +1,63 @@
+# Adapted from:
+# https://github.com/sixeyed/docker-windows-workshop/blob/master/part-5/db/Initialize-Database.ps1
+
+param(
+    [Parameter(Mandatory=$false)]
+    [string]$sa_password,
+
+    [Parameter(Mandatory=$false)]
+    [string]$data_path,
+
+    [Parameter(Mandatory=$true)]
+    [string]$db_name,
+
+    [Parameter(Mandatory=$false)]
+    [string]$secret_path
+)
+
 # start the service
 Write-Host 'Starting SQL Server'
 Start-Service MSSQL`$SQLEXPRESS
-$sa_password='_'
 
-$secretPath = $env:PASSWORD_PATH
-if (Test-Path $secretPath) {
-    $sa_password = Get-Content -Raw $secretPath
-	Write-Host 'Changing SA login credentials'
+# set sa password 
+if ($secret_path -ne "_" -and (Test-Path $secret_path)) {
+    $sa_password = Get-Content -Raw $secret_path
+} else {
+    Write-Host "PASSWORD_PATH is not set or SA password not found at: $secret_path"
+}
+
+if ($sa_password -ne "_") {
+    Write-Host 'Changing SA login credentials'
     $sqlcmd = "ALTER LOGIN sa with password='$sa_password'; ALTER LOGIN sa ENABLE;"
     Invoke-SqlCmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS" 
 }
-else {
-    Write-Host "SA password not found at: $secretPath"
-    return 1
+
+# attach database if files exist:
+$mdfPath = "$data_path\${db_name}_Data.mdf"
+$ldfPath = "$data_path\${db_name}_Log.ldf"
+
+if ((Test-Path $mdfPath) -eq $true) {
+    $sqlcmd = "CREATE DATABASE ${db_name} ON (FILENAME = N'$mdfPath')"    
+    if ((Test-Path $mdfPath) -eq $true) {
+        $sqlcmd =  "$sqlcmd, (FILENAME = N'$ldfPath')"
+    }
+    $sqlcmd = "$sqlcmd FOR ATTACH;"
+    Write-Verbose 'Data files exist - will create and attach database'
 }
+else {
+    # create database using the volume location:
+    $sqlcmd = 
+    "IF NOT EXISTS(SELECT 1 FROM sys.databases WHERE Name = '${db_name}') 
+      CREATE DATABASE ${db_name} ON 
+       PRIMARY ( NAME = N'${db_name}_Data', FILENAME = N'$mdfPath') 
+       LOG ON ( NAME = N'${db_name}_Log', FILENAME = N'$ldfPath' )"    
+    Write-Verbose 'Data files do not exist - will create new database'
+}
+
+Invoke-Sqlcmd -Query $sqlcmd -ServerInstance ".\SQLEXPRESS"
+Write-Verbose "Created/Attached $db_name database, data files at: $data_path"
+
+Write-Verbose "Started SQL Server."
 
 $lastCheck = (Get-Date).AddSeconds(-2) 
 while ($true) { 
